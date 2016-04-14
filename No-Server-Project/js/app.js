9774a7c565dcf33782d719fdd3c9f16932d78f19
Johnathan Lund <Johnathan Lund>
2016-04-14 17:00:39 -0600

    project rough draft with routes

---
+++ No-Server-Project/js/app.js
@@ -0,0 +1,28 @@
+angular.module('portfolioApp', ['ui.router'])
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/homePage');
+
+    $stateProvider
+        .state('home', {
+          url: "/homePage",
+          templateUrl:"./js/Home/homeTmpl.html",
+        })
+        .state('work', {
+          url: "/workPage",
+          templateUrl: "./js/work/workTmpl.html"
+        })
+        .state('inspiration', {
+          url: "/inspiration",
+          templateUrl: "./js/inspiration/inspirTmpl.html"
+        })
+        .state('knowledge', {
+          url: "/knowledge",
+          templateUrl: "./js/knowledge/knowTmpl.html"
+        })
+        .state('spartaAttack', {
+          url: "/spartaPage",
+          templateUrl: "./js/home/spartaTmpl.html"
+        })
+
+})
