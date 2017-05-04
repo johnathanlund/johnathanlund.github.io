92f68da1e75e5d048dde376eb2bbbab353c2fe5f
johnathanlund <jalshield@gmail.com>
2017-05-04 00:00:24 -0600

    Added a new project Told Handyman. Found and changed the source of the yellow background on Body.

--- js/Home/homeCtrl.js
+++ js/Home/homeCtrl.js
@@ -8,4 +8,36 @@ angular.module('appPortfolio').controller('homeCtrl', function ($scope) {
     this.hoverInfo = false;
   };
 
+  $scope.hoverInHandyman = function() {
+    this.hoverInfoHandyman = true;
+  };
+
+  $scope.hoverOutHandyman = function() {
+    this.hoverInfoHandyman = false;
+  };
+
+  $scope.hoverInTree = function() {
+    this.hoverInfoTree = true;
+  };
+
+  $scope.hoverOutTree = function() {
+    this.hoverInfoTree = false;
+  };
+
+  $scope.hoverInDevHousing = function() {
+    this.hoverInfoDevHousing = true;
+  };
+
+  $scope.hoverOutDevHousing = function() {
+    this.hoverInfoDevHousing = false;
+  };
+
+  $scope.hoverInPhotoshop = function() {
+    this.hoverInfoPhotoshop = true;
+  };
+
+  $scope.hoverOutPhotoshop = function() {
+    this.hoverInfoPhotoshop = false;
+  };
+
 });
